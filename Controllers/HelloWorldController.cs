1a4fe0f4ca9775eee5847ec52cc912a995a2c0ca
W0824662 <W0824662@ADL-A1201I-09>
2023-10-13 12:03:28 -0400

    updated index method and addnew folder helloworld and add new item Indec.cshtml and update the code

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,11 +8,15 @@ namespace MvcShoe.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        // updated index method
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
-
         // 
         // GET: /HelloWorld/Welcome/ 
 
@@ -23,9 +27,9 @@ namespace MvcShoe.Controllers
         //updated welcome method
         // GET: /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
