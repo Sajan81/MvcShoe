134e669bc08d52200d85cf246885e3e717937b90
W0824662 <W0824662@ADL-A1201I-09>
2023-10-13 15:53:49 -0400

    i have done modifications i have added images in it and added font to it and adedde images colour and all other stuff al in all i added css to enhance it looks and by which it llooked more elegent

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace MvcShoe.Controllers
         {
             return View();
         }
+        public IActionResult ABOUT()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
