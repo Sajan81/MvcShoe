1cf115830156b93248814590b2ee08d89a3a56f7
W0824662 <W0824662@ADL-A1201I-09>
2023-10-13 12:23:56 -0400

    i add a new folder named data and create new class and updated the code

---
+++ Data/MvcShoeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcShoe.Models;
+
+namespace MvcShoe.Data
+{
+    public class MvcShoeContext : DbContext
+    {
+        public MvcShoeContext(DbContextOptions<MvcShoeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shoe> Shoe { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcShoe.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcShoe
 {
@@ -24,8 +26,10 @@ namespace MvcShoe
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-        }
 
+            services.AddDbContext<MvcShoeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcShoeContext")));
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
