4374bd7c052b32258d3864dd3b6ba52f0214fe80
W0824662 <W0824662@ADL-A1201I-09>
2023-10-13 12:16:20 -0400

    i add new class and updated the code and add 6 fileds

---
+++ Models/Shoe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcShoe.Models
+{
+    public class Shoe
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LaunchDate { get; set; }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+    }
+}
